622465719e9d5ea2d69f1d35cc158cbd9cd11c11
D2007 <126488825+DereckAbraham@users.noreply.github.com>
2024-02-28 13:43:19 -0600

    Add files via upload

---
+++ js/mian.js
@@ -0,0 +1,62 @@
+let root = document.querySelector(".root")
+root.innerHTML = `
+<div class="cuadroGrande">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis officia fugiat et sequi culpa error optio dolores impedit minus dignissimos magnam ipsum, necessitatibus aliquam architecto, minima excepturi, pariatur vel?
+</div>
+<div class="cuadroContenedor">
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+<div class="conteInfo">
+    <div class="fto">
+        .
+    </div>
+    <div class="texto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, perferendis doloribus. Facilis dolor aut illo cumque distinctio totam nesciunt debitis. Inventore ea, excepturi labore mollitia doloremque vel! Voluptas, tempora dolorum?
+    </div>
+</div>
+
+</div>
+`;
